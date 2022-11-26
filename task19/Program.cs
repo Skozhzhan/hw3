b03719405e23681110fd4ed666f9a39b25da312a
Skozhzhan <106809865+Skozhzhan@users.noreply.github.com>
2022-11-26 16:02:26 +0300

    resolve task1

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Находим 1-ю цифру заданного числа 
+// Console.WriteLine("Задайте 5-ти значное число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int i = 10000;
+// if (i < a)
+//     while (i < 100000)
+//     {
+//         if (i > a) break;
+//         i = i + 1000;
+//     }
+// Console.WriteLine("Первая цифра числа " + i / 10000);
+
+// // Находим 5-ю цифру заданного числа
+// int b = a % 10;
+// int i5 = 0;
+// if (i5 <= b)
+//     while (i5 < 10)
+//     {
+//         if (i5 >= b) break;
+//         i5 = i5 + 1;
+//     }
+// Console.WriteLine("Пятая цифра числа " + i5);
+
+// if (i / 10000 == i5)
+// // Находим 2-ю цифру заданного числа
+// {
+//     int c = a / 1000 % 10;
+//     int i2 = 0;
+//     if (i2 <= c)
+//         while (i2 < 10)
+//         {
+//             if (i2 >= c) break;
+//             i2 = i2 + 1;
+//         }
+//     Console.WriteLine("Вторая цифра числа " + i2);
+//      // Находим 4-ю цифру заданного числа 
+//     int d = a / 10 % 10;
+//     int i4 = 0;
+//     if (i4 <= d)
+//         while (i4 < 10)
+//         {
+//             if (i4 >= c) break;
+//             i4 = i4 + 1;
+//         }
+//     Console.WriteLine("Четвертая цифра числа " + i4);
+//     if (i2 == i4)
+//     {
+//         Console.WriteLine("Палиндром");
+//     }
+
+// }
+
+// else
+// {
+//     Console.WriteLine("Не является Палиндромом");
+// }
+
+// другое решение
+Console.WriteLine("Задайте 5-ти значное число");
+int a = Convert.ToInt32(Console.ReadLine());
+bool Palindrom(int num)  // - логический метод
+{
+    return (num / 10000 == num % 10) && (num / 1000 % 10 == num / 10 % 10);
+    // сравниваем 1 и 5 цифру и 2 и 4 - если равны - палиндром
+}
+Console.WriteLine(Palindrom(a));
